372789156d68801c62dfbbb1ca334db0f226272f
nathandrapeza <drapeza@ualberta.ca>
2022-08-31 21:26:19 -0600

    added aesthetic foundations of app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react';
+import { AiOutlineSearch } from "react-icons/ai";
+import { useEffect, useState } from 'react';
+
+
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+  const [searchName, setSearchName] = useState('');
+  function changeBackgroundHover(e) {
+    e.target.style.background = '#444';
+  }
+
+  function changeBackgroundLeave(e) {
+    e.target.style.background = '#999';
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Name Location</h1>
+      <h2>Enter a name, and we'll try to guess where it's from!</h2>
+      <form className="searchbar">
+        <input className="textInput" type="text" name="name" placeholder='...'/>
+        <button onMouseEnter={changeBackgroundHover} onMouseLeave={changeBackgroundLeave}><AiOutlineSearch></AiOutlineSearch></button>
+      </form>
+    </>
   );
 }
 
